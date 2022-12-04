30b334843fd0c1834e7764926054aec6b5a2e382
Joel Jonsson <joel.jonsson@smartbear.com>
2022-12-04 06:09:34 +0100

    Solved 2022 day 4

---
+++ src/main/java/se/timotej/advent/y2022/Advent4b.java
@@ -0,0 +1,38 @@
+package se.timotej.advent.y2022;
+
+import se.timotej.advent.y2021.Util;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Advent4b {
+
+    public static void main(String[] args) throws IOException {
+        int svar = new Advent4b().calc(Online.get());
+        System.out.println("svar = " + svar);
+        Online.submit(svar);
+    }
+
+    private int calc(List<String> strs) {
+        int sum = 0;
+        for (String str : strs) {
+            List<Integer> ints = Util.findAllPositiveInts(str);
+            int overlap = 0;
+            if (ints.get(0) >= ints.get(2) && ints.get(0) <= ints.get(3)) {
+                overlap = 1;
+            }
+            if (ints.get(1) >= ints.get(2) && ints.get(1) <= ints.get(3)) {
+                overlap = 1;
+            }
+            if (ints.get(2) >= ints.get(0) && ints.get(2) <= ints.get(1)) {
+                overlap = 1;
+            }
+            if (ints.get(3) >= ints.get(0) && ints.get(3) <= ints.get(1)) {
+                overlap = 1;
+            }
+            sum += overlap;
+        }
+        return sum;
+    }
+
+}
